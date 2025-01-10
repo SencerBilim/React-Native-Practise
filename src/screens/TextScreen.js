a6f7c79718b9d2a75b2f944e519951abc07073d2
SencerBilim <sencer.bilim@gmail.com>
2025-01-10 15:10:33 +0300

    Testing

--- src/screens/TextScreen.js
+++ src/screens/TextScreen.js
@@ -1,14 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 
 import { View, Text, StyleSheet,TextInput } from "react-native"
 
 
 const TextScreen = () => {
+const [name, setName] = useState("");
+
     return <View>
-        <TextInput />
+        <Text>Password:</Text>
+        <TextInput style={styles.input}
+        autoCapitalize="none"
+        autoCorrect={false}
+        value={name}
+        onChangeText={(newValue) => setName(newValue)} />
+        {name.length > 0 && name.length < 5 ? (
+    <Text style={{ color: 'red' }}>Password should be longer than 5 characters</Text>
+) : null}
     </View>
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    input: {
+        margin: 15,
+        borderColor: "black",
+        borderWidth: 1,
+        padding: 5
+    }
+});
 
 export default TextScreen;
